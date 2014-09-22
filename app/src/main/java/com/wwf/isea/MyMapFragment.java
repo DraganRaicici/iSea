a7fb9c58ec498077ac0cba4534721c805658427c
Sarac Cristian <sarac.cristian@gmail.com>
2014-09-22 20:28:19 +0200

    Scroll view added And UI for first page done

--- app/src/main/java/com/wwf/isea/MyMapFragment.java
+++ app/src/main/java/com/wwf/isea/MyMapFragment.java
@@ -6,13 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 
 
 /**
@@ -21,9 +15,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class MyMapFragment extends Fragment{
 
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
+
+   // private GoogleMap map;
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     public static MyMapFragment newInstance(int sectionNumber) {
@@ -39,22 +32,8 @@ public class MyMapFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.ic_launcher)));
-
-        // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+       // map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+             //   .getMap();
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);

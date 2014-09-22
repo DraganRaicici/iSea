65404eac1e9231d87353da5410f5bac1fee0c240
RDGary <dragan.raicici@gmail.com>
2014-09-22 19:37:23 +0200

    Modifications in AndroidManifest (added permissions and features) Added new class: MyMapFragment Added fragment_map.xml for the map fragment

--- app/src/main/java/com/wwf/isea/MyMapFragment.java
+++ app/src/main/java/com/wwf/isea/MyMapFragment.java
@@ -23,7 +23,18 @@ public class MyMapFragment extends Fragment{
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
+    public static MyMapFragment newInstance(int sectionNumber) {
+        MyMapFragment fragment = new MyMapFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MyMapFragment() {
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {

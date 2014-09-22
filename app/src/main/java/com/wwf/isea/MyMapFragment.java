cf23e1386db7918c296a2ffbfc72832f67feb469
RDGary <dragan.raicici@gmail.com>
2014-09-22 21:12:23 +0200

    fucking map working

--- app/src/main/java/com/wwf/isea/MyMapFragment.java
+++ app/src/main/java/com/wwf/isea/MyMapFragment.java
@@ -2,21 +2,25 @@ package com.wwf.isea;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
  * Created by rdgary on 9/22/14.
- * hello
+ *
  */
 public class MyMapFragment extends Fragment{
 
 
-   // private GoogleMap map;
+    private GoogleMap mMap;
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     public static MyMapFragment newInstance(int sectionNumber) {
@@ -29,14 +33,19 @@ public class MyMapFragment extends Fragment{
 
     public MyMapFragment() {
     }
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-       // map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-             //   .getMap();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView= inflater.inflate(R.layout.fragment_map, container, false);
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        int latitude=40;
+        int longitude=20;
+        int zoom=7;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), zoom));
+
+
+        return rootView;
     }
-
 }
